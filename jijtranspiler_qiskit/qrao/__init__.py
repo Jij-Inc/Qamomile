301585029f0a91228036fdeb6ada005be4d9330a
Yu Yamashiro <yuyamashiro89@gmail.com>
2023-06-21 12:41:16 +0900

    bug fix

---
+++ jijtranspiler_qiskit/qrao/__init__.py
@@ -0,0 +1,9 @@
+from .graph_coloring import greedy_graph_coloring
+from .qrao31 import color_group_to_qrac_encode, qrac31_encode
+
+
+__all__ = [
+    "greedy_graph_coloring",
+    "color_group_to_qrac_encode",
+    "qrac31_encode",
+]
--- jijtranspiler_qiskit/qrao/qrao31.py
+++ jijtranspiler_qiskit/qrao/qrao31.py
@@ -11,7 +11,21 @@ class Pauli(enum.Enum):
     Z = enum.auto()
 
 
-def color_group_for_qrac_encode(color_group: dict[int, list[int]]):
+def color_group_to_qrac_encode(color_group: dict[int, list[int]]) -> dict[int, tuple[int, Pauli]]:
+    """qrac encode
+
+    Args:
+        color_group (dict[int, list[int]]): key is color (qubit's index). value is list of bit's index.
+
+    Returns:
+        dict[int, tuple[int, Pauli]]: key is bit's index. value is tuple of qubit's index and Pauli operator kind.
+
+    Examples:
+        >>> color_group = {0: [0, 1, 2], 1: [3, 4], 2: [6,]}
+        >>> color_group_for_qrac_encode(color_group)
+        {0: (0, <Pauli.Z: 3>), 1: (0, <Pauli.X: 1>), 2: (0, <Pauli.Y: 2>), 3: (1, <Pauli.Z: 3>), 4: (1, <Pauli.X: 1>), 6: (2, <Pauli.Z: 3>)}
+
+    """
     qrac31 = {}
     pauli_ope = [Pauli.Z, Pauli.X, Pauli.Y]
     for color, group in color_group.items():
@@ -20,76 +34,60 @@ def color_group_for_qrac_encode(color_group: dict[int, list[int]]):
     return qrac31
 
 
+def create_pauli_term(operators: list[Pauli], indices: list[int]):
+    n_qubit = max(indices) + 1
+    z_p = np.zeros(n_qubit, dtype=bool)
+    x_p = np.zeros(n_qubit, dtype=bool)
+    for ope, idx in zip(operators, indices):
+        if ope == Pauli.X:
+            x_p[idx] = True
+        elif ope == Pauli.Y:
+            x_p[idx] = True
+            z_p[idx] = True
+        elif ope == Pauli.Z:
+            z_p[idx] = True
+    return qk_ope.SparsePauliOp(qk_ope.Pauli((z_p, x_p)))
+
+
 def qrac31_encode(
     qubo: dict[tuple[int, int], float],
     color_group: dict[int, list[int]]
 ) -> tuple[qk_ope.SparsePauliOp, float]:
     ising = qubo_to_ising(qubo)
 
-    encoded_ope = color_group_for_qrac_encode(color_group)
+    encoded_ope = color_group_to_qrac_encode(color_group)
 
     pauli_terms: list[qk_ope.SparsePauliOp] = []
 
     offset = ising.constant
     n_qubit = len(color_group)
-    zero = np.zeros(n_qubit, dtype=bool)
-
-    operator_parity = {Pauli.X: (False, True), Pauli.Y: (True, False), Pauli.Z: (True, False)}
 
     # convert linear parts of the objective function into Hamiltonian.
-    for idx, coef in ising.linear.items():
-        if coef == 0.0:
+    for idx, coeff in ising.linear.items():
+        if coeff == 0.0:
             continue
  
-        weight = coef
-        color, idx_pauli = encoded_ope[idx]
-        z_parity, x_parity = operator_parity[idx_pauli]
-        if z_parity:
-            z_p = zero.copy()
-            z_p[color] = True
-        else:
-            z_p = zero
-        if x_parity:
-            x_p = zero.copy()
-            x_p[color] = True
-        else:
-            x_p = zero
+        color, pauli_kind = encoded_ope[idx]
+        pauli_operator = create_pauli_term([pauli_kind], [color])
 
-        pauli_terms.append(qk_ope.SparsePauliOp(qk_ope.Pauli((z_p, x_p)), -weight))
-        offset += weight
+        pauli_terms.append(qk_ope.SparsePauliOp(pauli_operator), 1/np.sqrt(3)*coeff)
 
     # create Pauli terms
     for (i, j), coeff in ising.quad.items():
         if coeff == 0.0:
             continue
 
-        weight = coeff
-
         if i == j:
-            offset += weight
+            offset += coeff
             continue
 
-        color_i, pauli_i_index = encoded_ope[i]
-        zi_p, xi_p = operator_parity[pauli_i_index]
+        color_i, pauli_kind_i = encoded_ope[i]
 
-        color_j, pauli_j_index = encoded_ope[j]
-        zj_p, xj_p = operator_parity[pauli_j_index]
+        color_j, pauli_kind_j = encoded_ope[j]
 
-        if zi_p or zj_p:
-            z_p = zero.copy()
-            z_p[color_i] = zi_p
-            z_p[color_j] = zj_p
-        else:
-            z_p = zero
+        pauli_ope = create_pauli_term([pauli_kind_i, pauli_kind_j], [color_i, color_j])
 
-        if xi_p or xj_p:
-            x_p = zero.copy()
-            x_p[color_i] = xi_p
-            x_p[color_j] = xj_p
-        else:
-            x_p = zero
-
-        pauli_terms.append(qk_ope.SparsePauliOp(qk_ope.Pauli((z_p, x_p)), weight))
+        pauli_terms.append(qk_ope.SparsePauliOp(pauli_ope, 1/3*coeff))
 
     if pauli_terms:
         # Remove paulis whose coefficients are zeros.
