7cfa045767aef583782d8c09e41fd0c4e1ba0a44
Jacomichi <h.matsuyama@j-ij.com>
2024-06-06 20:39:06 +0900

    fix compiled hamiltonian test

--- tests/core/compile/test_compiled_hamiltonian.py
+++ tests/core/compile/test_compiled_hamiltonian.py
@@ -9,14 +9,14 @@ def test_CompiledHamiltonian():
     N = jm.Placeholder("N")
     i = jm.Element("i", belong_to=(0,N))
     expr = X[i]
-    compiled_hamiltonian = CompiledHamiltonian(substituted_hamiltonian=SubstitutedQuantumExpression(coeff={PauliOperator(PauliType.X, 0): 1.0}, constant=1.0, order=1), hamiltonian=Hamiltonian(expr), data=jtc.InstanceData({'N':n},{},[],{}),var_map = jtc.VariableMap({}, var_num=0, integer_bound={}),  deci_var_shape={})
+    compiled_hamiltonian = CompiledHamiltonian(substituted_hamiltonian=SubstitutedQuantumExpression(coeff={PauliOperator(PauliType.X, 0): 1.0}, constant=1.0, order=1), hamiltonian=Hamiltonian(expr), data=jtc.InstanceData({'N':n},{},[],{}),var_map = jtc.VariableMap({}, var_num=0, integer_bound={}),  qubit_index_map={})
     assert compiled_hamiltonian.substituted_hamiltonian.coeff[PauliOperator(PauliType.X, 0)] == 1.0
     assert compiled_hamiltonian.substituted_hamiltonian.constant == 1.0
     assert compiled_hamiltonian.substituted_hamiltonian.order == 1
     assert jm.is_same(compiled_hamiltonian.hamiltonian.hamiltonian, expr)
     assert compiled_hamiltonian.data.tensor_data == {'N':n}
     assert compiled_hamiltonian.var_map == jtc.VariableMap({}, var_num=0, integer_bound={})
-    assert compiled_hamiltonian.deci_var_shape == {}
+    assert compiled_hamiltonian.qubit_index_map == {}
 
 
 
