a02d1fb902f841da0c27c5c7f9dddde4993c56e0
Yu Yamashiro <yuyamashiro89@gmail.com>
2024-11-27 16:15:55 +0900

    use singlefile

--- docs/qamomile-lp/vite.config.js
+++ docs/qamomile-lp/vite.config.js
@@ -1,9 +1,10 @@
 // vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteSingleFile()],
   build: {
     assetsInlineLimit: 0, // 画像のインライン化を防ぐ
     // CSS をインライン化
