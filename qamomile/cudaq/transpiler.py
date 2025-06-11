6eac98e552151256ad30aba38d4688e385679e3d
ksk-jij <keisuke.sato@j-ij.com>
2025-06-11 12:38:30 +0900

    #142 Implement transpile_hamiltonian for CUDA-Q

--- qamomile/cudaq/transpiler.py
+++ qamomile/cudaq/transpiler.py
@@ -20,6 +20,8 @@ Requirements:
 
 import collections
 
+import cudaq
+
 import qamomile
 import qamomile.core.bitssample as qm_bs
 from qamomile.core.transpiler import QuantumSDKTranspiler
@@ -30,8 +32,41 @@ class CudaqTranspiler(QuantumSDKTranspiler[tuple[collections.Counter[int], int]]
     A transpiler class for converting Qamomile Hamiltonians to CUDA-Q-compatible Hamiltonians.
     """
 
-    def transpile_hamiltonian(self, operator: qamomile.core.operator.Hamiltonian):
-        pass
+    def transpile_hamiltonian(
+        self, operator: qamomile.core.operator.Hamiltonian
+    ) -> cudaq.SpinOperator:
+        """
+        Converts a Qamomile Hamiltonian to a CUDA-Q Hamiltonian.
+
+        Args:
+            operator (qamomile.core.operator.Hamiltonian): The Qamomile Hamiltonian to be converted.
+
+        Returns:
+            cudaq.SpinOperator: The corresponding CUDA-Q Hamiltonian.
+        """
+        # Iterate over the terms in the given Qamomile Hamiltonian.
+        hamltonian = 0
+        for term, coeff in operator.terms.items():
+
+            # Create each term for the CUDA-Q hamiltonian.
+            hamltonian_term = coeff
+            for pauli in term:
+                match pauli.pauli:
+                    case qamomile.core.operator.Pauli.X:
+                        hamltonian_term *= cudaq.spin.x(pauli.index)
+                    case qamomile.core.operator.Pauli.Y:
+                        hamltonian_term *= cudaq.spin.y(pauli.index)
+                    case qamomile.core.operator.Pauli.Z:
+                        hamltonian_term *= cudaq.spin.z(pauli.index)
+                    case _:
+                        raise NotImplementedError(
+                            "Only Pauli X, Y, and Z are supported"
+                        )
+
+            # Add the term to the hamiltonian.
+            hamltonian += hamltonian_term
+
+        return hamltonian
 
     def transpile_circuit(self, qamomile_circuit: qamomile.core.circuit.QuantumCircuit):
         pass
