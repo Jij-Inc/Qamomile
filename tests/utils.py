3f5c6dff5fa2e06bf5b7dcd042f4b2f8a2499651
ksk-jij <keisuke.sato@j-ij.com>
2025-06-13 19:16:31 +0900

    #142 Add utilities for testing

---
+++ tests/utils.py
@@ -0,0 +1,42 @@
+from typing import Final
+import numpy as np
+
+KET_0: Final[np.ndarray] = np.array([[1], [0]])
+KET_1: Final[np.ndarray] = np.array([[0], [1]])
+
+
+I_MATRIX: Final[np.ndarray] = np.identity(2)
+H_MATRIX: Final[np.ndarray] = np.array([[1, 1], [1, -1]]) / np.sqrt(2)
+X_MATRIX: Final[np.ndarray] = np.array([[0, 1], [1, 0]])
+
+CX_MATRIX: Final[np.ndarray] = np.array(
+    [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 1], [0, 0, 1, 0]]
+)
+
+CCX_MATRIX: Final[np.ndarray] = np.array(
+    [
+        [1, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0, 0, 1, 0],
+    ]
+)
+
+
+RX_MATRIX = lambda theta: np.array(
+    [
+        [np.cos(theta / 2), -1j * np.sin(theta / 2)],
+        [-1j * np.sin(theta / 2), np.cos(theta / 2)],
+    ]
+)
+
+
+def take_tensor_product(*args):
+    result_state = args[0]
+    for mat in args[1:]:
+        result_state = np.kron(result_state, mat)
+    return result_state
