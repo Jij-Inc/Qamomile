d8faeb2dc2a334e4c02769e2120bf9e21d689d9e
ksk-jij <keisuke.sato@j-ij.com>
2025-06-13 16:36:48 +0900

    #142 Write __init__ for CUDA-Q

---
+++ qamomile/cudaq/__init__.py
@@ -0,0 +1,4 @@
+from qamomile.cudaq.transpiler import CudaqTranspiler
+from qamomile.cudaq.exceptions import QamomileCudaqTranspileError
+
+__all__ = ["CudaqTranspiler", "QamomileCudaqTranspileError"]
