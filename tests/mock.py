10421422898b16932479ed09c0a5194755d1a51f
ksk-jij <keisuke.sato@j-ij.com>
2025-06-16 18:18:48 +0900

    #142 Add tests for parameter_converter for CUDA-Q

---
+++ tests/mock.py
@@ -0,0 +1,10 @@
+class UnsupportedBinaryOpeKind:
+    """Mock class for an unsupported binary operation kind."""
+
+    pass
+
+
+class UnsupportedParam:
+    """Mock class for an unsupported parameter type."""
+
+    pass
