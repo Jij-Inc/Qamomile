2ff82b562c0014f077712c320afc09b07f340915
ksk-jij <keisuke.sato@j-ij.com>
2025-06-16 17:33:15 +0900

    #142 Add parameter_converter for CUDA-Q

--- qamomile/cudaq/__init__.py
+++ qamomile/cudaq/__init__.py
@@ -1,4 +1,6 @@
 from qamomile.cudaq.transpiler import CudaqTranspiler
 from qamomile.cudaq.exceptions import QamomileCudaqTranspileError
+from qamomile.cudaq.parameter_converter import convert_parameter
 
-__all__ = ["CudaqTranspiler", "QamomileCudaqTranspileError"]
+
+__all__ = ["CudaqTranspiler", "QamomileCudaqTranspileError", "convert_parameter"]
