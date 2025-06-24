bb15e3a3fee0dec270d58eb0cd7aa384781b45b8
ksk-jij <keisuke.sato@j-ij.com>
2025-06-24 13:16:33 +0900

    #160 Remove the same concept tests

--- tests/core/test_bitssample.py
+++ tests/core/test_bitssample.py
@@ -244,21 +244,4 @@ def test_total_samples(samples, expected_total):
     assert sample_set.total_samples() == expected_total
 
 
-def test_from_int_counts_with_larger_bit_length():
-    int_counts = {0: 1, 15: 1}  # 15 is 1111 in binary
-    sample_set = BitsSampleSet.from_int_counts(int_counts, bit_length=5)
-    assert all(len(sample.bits) == 5 for sample in sample_set.bitarrays)
-    assert any(sample.bits == [0, 0, 0, 0, 0] for sample in sample_set.bitarrays)
-    assert any(sample.bits == [1, 1, 1, 1, 0] for sample in sample_set.bitarrays)
-
-
-def test_get_most_common_with_ties():
-    samples = [BitsSample(2, [0, 0]), BitsSample(2, [0, 1]), BitsSample(1, [1, 0])]
-    sample_set = BitsSampleSet(samples)
-    most_common = sample_set.get_most_common(3)
-    assert len(most_common) == 3
-    assert most_common[0].num_occurrences == most_common[1].num_occurrences == 2
-    assert most_common[2].num_occurrences == 1
-
-
 # <<< BitsSampleSet <<<
