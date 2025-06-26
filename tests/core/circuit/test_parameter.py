1ce2d51b70b613a1ef1ca864a20a36f19e72d5c1
ksk-jij <keisuke.sato@j-ij.com>
2025-06-26 18:33:54 +0900

    #166 Update tests for basic arithmetic

--- tests/core/circuit/test_parameter.py
+++ tests/core/circuit/test_parameter.py
@@ -74,39 +74,11 @@ def test_add_with_unsupported_type(other):
     """Add a ParameterExpressionChildMock instance with an unsupported type.
 
     Check if
-    1. TypeError is raised.
+    1. the returned value is NotImplemented.
     """
     expr = ParameterExpressionChildMock()
 
-    with pytest.raises(ValueError):
-        expr + other
-
-
-def test_radd():
-    """Right add two of ParameterExpressionChildMock instances.
-
-    Check if
-    1. expr1 is BinaryOperator.
-    2. expr1's left is ParameterExpressionChildMock.
-    3. expr1's right is the same as the expr2.
-    4. expr1's kind is BinaryOpeKind.ADD.
-    """
-    expr1 = ParameterExpressionChildMock()
-    expr2 = ParameterExpressionChildMock()
-
-    expr1 += expr2
-
-    print(type(expr1))
-    print(expr1)
-
-    # 1. expr1 is BinaryOperator.
-    assert isinstance(expr1, BinaryOperator)
-    # 2. expr1's left is ParameterExpressionChildMock.
-    assert isinstance(expr1.left, ParameterExpressionChildMock)
-    # 3. expr1's right is the same as the expr2.
-    assert expr1.right == expr2
-    # 4. expr1's kind is BinaryOpeKind.ADD.
-    assert expr1.kind == BinaryOpeKind.ADD
+    assert expr + other == NotImplemented
 
 
 @pytest.mark.parametrize("other", [1, 1.0, np.int64(1), np.float64(1.0)])
@@ -114,39 +86,38 @@ def test_radd_with_real(other):
     """Right add a ParameterExpressionChildMock instance with a real number.
 
     Check if
-    1. expr is BinaryOperator.
-    2. expr's left is ParameterExpressionChildMock.
-    3. expr's right is Value.
-    4. expr's right's value is the same as the given other.
-    5. expr's kind is BinaryOpeKind.ADD.
+    1. the returned value is BinaryOperator.
+    2. the returned value's left is expr.
+    3. the returned value's right is Value.
+    4. the returned value's right's value is the same as the given other.
+    5. the returned value's kind is BinaryOpeKind.ADD.
     """
     expr = ParameterExpressionChildMock()
 
-    expr += other
+    result = other + expr
 
-    # 1. expr is BinaryOperator.
-    assert isinstance(expr, BinaryOperator)
-    # 2. expr's left is ParameterExpressionChildMock.
-    assert isinstance(expr.left, ParameterExpressionChildMock)
-    # 3. expr's right is Value.
-    assert isinstance(expr.right, Value)
-    # 4. expr's right's value is the same as the given other.
-    assert expr.right.value == other
-    # 5. expr's kind is BinaryOpeKind.ADD.
-    assert expr.kind == BinaryOpeKind.ADD
+    # 1. the returned value is BinaryOperator.
+    assert isinstance(result, BinaryOperator)
+    # 2. the returned value's left is expr.
+    assert result.left == expr
+    # 3. the returned value's right is Value.
+    assert isinstance(result.right, Value)
+    # 4. the returned value's right's value is the same as the given other.
+    assert result.right.value == other
+    # 5. the returned value's kind is BinaryOpeKind.ADD.
+    assert result.kind == BinaryOpeKind.ADD
 
 
 @pytest.mark.parametrize("other", ["1", [1, 2], 1j])
-def test_aadd_with_unsupported_type(other):
+def test_radd_with_unsupported_type(other):
     """Right add a ParameterExpressionChildMock instance with an unsupported type.
 
     Check if
-    1. TypeError is raised.
+    1. the returned value is NotImplemented.
     """
     expr = ParameterExpressionChildMock()
 
-    with pytest.raises(ValueError):
-        expr += other
+    assert other + expr == NotImplemented
 
 
 def test_mul():
@@ -200,58 +171,31 @@ def test_mul_with_real(other):
     assert result.kind == BinaryOpeKind.MUL
 
 
-def test_rmul():
-    """Rigth mul two of ParameterExpressionChildMock instances.
-
-    Check if
-    1. expr1 is BinaryOperator.
-    2. expr1's left is ParameterExpressionChildMock.
-    3. expr1's right is the same as the expr2.
-    4. expr1's kind is BinaryOpeKind.MUL.
-    """
-    expr1 = ParameterExpressionChildMock()
-    expr2 = ParameterExpressionChildMock()
-
-    expr1 *= expr2
-
-    print(type(expr1))
-    print(expr1)
-
-    # 1. expr1 is BinaryOperator.
-    assert isinstance(expr1, BinaryOperator)
-    # 2. expr1's left is ParameterExpressionChildMock.
-    assert isinstance(expr1.left, ParameterExpressionChildMock)
-    # 3. expr1's right is the same as the expr2.
-    assert expr1.right == expr2
-    # 4. expr1's kind is BinaryOpeKind.MUL.
-    assert expr1.kind == BinaryOpeKind.MUL
-
-
 @pytest.mark.parametrize("other", [1, 1.0, np.int64(1), np.float64(1.0)])
 def test_rmul_with_real(other):
     """Right mul a ParameterExpressionChildMock instance with a real number.
 
     Check if
-    1. expr is BinaryOperator.
-    2. expr's right is ParameterExpressionChildMock.
-    3. expr's left is Value.
-    4. expr's left's value is the same as the given other.
-    5. expr's kind is BinaryOpeKind.MUL.
+    1. the returned value is BinaryOperator.
+    2. the returned value's right is the same as expr1.
+    3. the returned value's left is Value.
+    4. the returned value's left's value is the same as the given other.
+    5. the returned value's kind is BinaryOpeKind.MUL.
     """
     expr = ParameterExpressionChildMock()
 
-    expr *= other
+    result = other + expr
 
-    # 1. expr is BinaryOperator.
-    assert isinstance(expr, BinaryOperator)
-    # 2. expr's right is ParameterExpressionChildMock.
-    assert isinstance(expr.right, ParameterExpressionChildMock)
-    # 3. expr's left is Value.
-    assert isinstance(expr.left, Value)
-    # 4. expr's left's value is the same as the given other.
-    assert expr.left.value == other
-    # 5. expr's kind is BinaryOpeKind.MUL.
-    assert expr.kind == BinaryOpeKind.MUL
+    # 1. the returned value is BinaryOperator.
+    assert isinstance(result, BinaryOperator)
+    # 2. the returned value's right is the same as expr1.
+    assert result.right == expr
+    # 3. the returned value's left is Value.
+    assert isinstance(result.left, Value)
+    # 4. the returned value's left's value is the same as the given other.
+    assert result.left.value == other
+    # 5. the returned value's kind is BinaryOpeKind.MUL.
+    assert result.kind == BinaryOpeKind.MUL
 
 
 def test_neg():
